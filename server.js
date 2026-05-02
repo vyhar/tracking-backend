d8ad765d9c73c30dd62f6f935e648276f7150f46
vyhar <justin@vyhar.com>
2026-05-01 22:07:37 -0400

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const PORT = process.env.PORT || 3000;
 
 // ✅ Allow your GitHub Pages domain (replace with yours later)
 app.use(cors({
-  origin: "*", // change to your GitHub Pages URL later
+  origin: "https://vyhar.github.io/", // change to your GitHub Pages URL later
 }));
 
 app.use(express.json());
